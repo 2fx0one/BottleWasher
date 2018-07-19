98fc76d60b42a48799f7036950acb5d261f2509b
2fx0one <2fx0one@gmail.com>
2018-07-19 13:11:56 +0800

    commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private GameManager _instance;
+    public GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+	private void Awake()
+	{
+        _instance = this;
+	}
+
+    public int xCol;
+    public int yRow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
