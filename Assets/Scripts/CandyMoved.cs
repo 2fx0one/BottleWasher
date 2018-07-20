9a2f0f682ceece43b0a08be62fb6b6abf65e95f6
2fx0one <2fx0one@gmail.com>
2018-07-20 21:23:26 +0800

    candy barrier

--- Assets/Scripts/CandyMoved.cs
+++ Assets/Scripts/CandyMoved.cs
@@ -6,16 +6,44 @@ public class CandyMoved : MonoBehaviour
 {
 	private CandyObject _candyObject;
 
+	private IEnumerator moveCoroutine;
+	
 	private void Awake()
 	{
 		_candyObject = GetComponent<CandyObject>();
 	}
 
-	public void Move(int x, int y)
+	public void Move(int x, int y, float time)
+	{
+		if (moveCoroutine != null)
+		{
+			StopCoroutine(moveCoroutine);
+		}
+
+		moveCoroutine = MoveCoroutine(x, y, time);
+		StartCoroutine(moveCoroutine);
+//		_candyObject.X = x;
+//		_candyObject.Y = y;
+//        _candyObject.transform.position = _candyObject._gameManager.CorrectPostion(x, y);
+	}
+
+	private IEnumerator MoveCoroutine(int x, int y, float time)
 	{
 		_candyObject.X = x;
 		_candyObject.Y = y;
-        _candyObject.transform.position = _candyObject._gameManager.CorrectPostion(x, y);
+
+		Vector3 startPosition = transform.position;
+		Vector3 endPosition = _candyObject._gameManager.CorrectPostion(x, y);
+
+		for (float t = 0; t < time; t += Time.deltaTime)
+		{
+			_candyObject.transform.position
+				= Vector3.Lerp(startPosition, endPosition, t / time);
+			yield return 0; //每帧
+		}
+		
+		//保证位置一定对。
+		_candyObject.transform.position = _candyObject._gameManager.CorrectPostion(x, y);
 	}
 	
 	
